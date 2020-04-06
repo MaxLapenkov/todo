ac217797e2fff6b8f15b57f0de02f792fac6dea1
Maxel <gigaabait@yandex.ru>
2020-04-06 18:54:13 +0400

    b
    build

---
+++ build/precache-manifest.681ab77842eb9ad1396ca48d4362f691.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c603169041a7097882c7950cf7f1a12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d359782ab7084f04e79d",
+    "url": "/static/css/main.8a022c10.chunk.css"
+  },
+  {
+    "revision": "7364c52c36f102127d8c",
+    "url": "/static/js/2.dacf4058.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dacf4058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d359782ab7084f04e79d",
+    "url": "/static/js/main.c49bad20.chunk.js"
+  },
+  {
+    "revision": "3d5e8122786b82ae80e1",
+    "url": "/static/js/runtime-main.ecc82796.js"
+  }
+]);
