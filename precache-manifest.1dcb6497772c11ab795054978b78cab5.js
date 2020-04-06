4ea5ab25d099d5d32b2a09b7be8341cc0829e6c3
Maxel <gigaabait@yandex.ru>
2020-04-06 23:06:37 +0400

    Updates

---
+++ precache-manifest.1dcb6497772c11ab795054978b78cab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68fed9597a48c940047afc2f6dbee90a",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "d359782ab7084f04e79d",
+    "url": "/todo/static/css/main.8a022c10.chunk.css"
+  },
+  {
+    "revision": "7364c52c36f102127d8c",
+    "url": "/todo/static/js/2.dacf4058.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.dacf4058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d359782ab7084f04e79d",
+    "url": "/todo/static/js/main.c49bad20.chunk.js"
+  },
+  {
+    "revision": "0fad5e3480eda5bbef65",
+    "url": "/todo/static/js/runtime-main.64f6f58d.js"
+  }
+]);
